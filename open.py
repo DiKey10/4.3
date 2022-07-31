75a0afd2824788ca3700702685b81e53442c911e
DiKey10 <kd930603@gmail.com>
2022-07-31 14:21:10 +0200

    na szybko nowe zmiany

---
+++ open.py
@@ -0,0 +1,7 @@
+with open("names.txt", 'r') as read_file:
+  for line in read_file.read().splitlines():
+    print(line)
+
+new_name = "Luke"
+with open("new_names.txt", 'w') as write_file:
+    write_file.write(new_name)
