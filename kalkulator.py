ff44056ba77eb45611b4d434862389fc22325fab
DiKey10 <kd930603@gmail.com>
2022-07-31 16:16:19 +0200

    Sprawna wersja na pushach

--- kalkulator.py
+++ kalkulator.py
@@ -1,12 +1,12 @@
 import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 print("Hej, pora na małe obliczenia")
-dzialanie = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-pierwsza = input("Skoro wiemy co bedziemy robic prosze podaj pierwsza liczbe :")
-druga = input("Dobra jeszcze tylko druga liczba :")
-print(pierwsza)
-print(druga)
-print(dzialanie)
+dzialanie = float(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+pierwsza = float(input("Skoro wiemy co bedziemy robic prosze podaj pierwsza liczbe :"))
+druga = float(input("Dobra jeszcze tylko druga liczba :"))
+
 
 
 def dodawanie(pierwsza, druga):
@@ -18,15 +18,39 @@ def mnozenie(pierwsza, druga):
 def dzielenie(pierwsza, druga):
     return pierwsza / druga
 
+dod=dodawanie(pierwsza, druga)
+ode=odejmowanie(pierwsza, druga)
+mno=mnozenie(pierwsza, druga)
+dzie=dzielenie(pierwsza, druga)
 
 def kalkulator(dzialanie):
     if dzialanie == 1:
-        print(dodawanie)
+        logging.debug("Dodaję  " + str(pierwsza) + " i "+ str(druga))
+        print(f"Wynik to " + str(dod))
     if dzialanie == 2:
-        print(odejmowanie)
+        logging.debug("Odejmuje  " + str(pierwsza) + " i "+ str(druga))
+        print(f"Wynik to " + str(ode))
     if dzialanie == 3:
-        print(mnozenie)
+        logging.debug("Mnoze  " + str(pierwsza) + " i "+ str(druga))
+        print(f"Wynik to " + str(mno))
     if dzialanie == 4:
-        print(dzielenie)
+        logging.debug("Dziele  " + str(pierwsza) + " i "+ str(druga))
+        print(f"Wynik to " + str(dzie))
 
 kalkulator(dzialanie)
+
+
+"""
+if dzialanie == 1:
+    print("Dodaję "+ str(pierwsza) + " i "+ str(druga))
+    print(f"Wynik to " + str(pierwsza + druga))
+if dzialanie == 2:
+    print("Odejmuje "+ str(pierwsza) + " i "+ str(druga))
+    print(f"Wynik to " + str(pierwsza + druga))
+if dzialanie == 3:
+    print("Mnoze "+ str(pierwsza) + " i "+ str(druga))
+    print(f"Wynik to " + str(pierwsza + druga))
+if dzialanie == 4:
+    print("Dziele "+ str(pierwsza) + " i "+ str(druga))
+    print(f"Wynik to " + str(pierwsza + druga))
+"""
